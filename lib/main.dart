965f79dd3300cad71c3dbb01ea125661f171b848
Polat Avcı <polatavci2222@gmail.com>
2025-03-01 17:22:39 +0300

    CartPage ve Cart_items birbirinden ayrıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yazilim_muh_proje/pages/login_page.dart';
+import 'package:yazilim_muh_proje/pages/cart_page.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: CartPage()));
 }
