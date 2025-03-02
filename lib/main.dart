607d17f23b09ccf6a7bb60ec1096c165f8373c9b
Polat Avcı <polatavci2222@gmail.com>
2025-03-02 20:31:00 +0300

    AddressPage sayfa görünmeme sorunu çözüldü AddressPage'e görünüm değişiklikleri yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:yazilim_muh_proje/pages/address_page.dart';
-import 'package:yazilim_muh_proje/pages/cart_page.dart';
-import 'package:yazilim_muh_proje/pages/login_page.dart';
 
 void main() {
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: AddressPage()));
--- lib/pages/address_page.dart
+++ lib/pages/address_page.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:yazilim_muh_proje/Models/address.dart';
-import 'package:yazilim_muh_proje/pages/add_address_page.dart';
+import 'package:yazilim_muh_proje/components/button.dart';
+import 'package:yazilim_muh_proje/components/custom_text_field.dart';
 
 class AddressPage extends StatefulWidget {
   const AddressPage({super.key});
@@ -15,11 +16,33 @@ class _AddressPageState extends State<AddressPage> {
     Address(id: "2", address: "Atatürk Bulvarı No:12", sehir: "Ankara"),
     Address(id: "3", address: "Konak Mah. 123. Sokak", sehir: "İzmir"),
   ];
+  final _cityController = TextEditingController();
+  final _addressController = TextEditingController();
 
-  void _addNewAddress(Address newAddress) {
-    setState(() {
+  void _saveAddress() {
+    if (_addressController.text.isNotEmpty && _cityController.text.isNotEmpty) {
+      final newAddress = Address(
+        id: DateTime.now().millisecondsSinceEpoch.toString(),
+        address: _addressController.text,
+        sehir: _cityController.text,
+      );
+
       adresler.add(newAddress);
-    });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Adresiniz kaydedildi"),
+          showCloseIcon: true,
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Lütfen tüm alanları doldurun."),
+          showCloseIcon: true,
+        ),
+      );
+    }
   }
 
   void _deleteAddress(Address address) {
@@ -27,9 +50,9 @@ class _AddressPageState extends State<AddressPage> {
       adresler.removeWhere((a) => a.id == address.id);
     });
 
-    ScaffoldMessenger.of(
-      context,
-    ).showSnackBar(const SnackBar(content: Text('Adres silindi')));
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(content: Text('Adres silindi'), showCloseIcon: true),
+    );
   }
 
   @override
@@ -42,25 +65,72 @@ class _AddressPageState extends State<AddressPage> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
         ),
         actions: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder:
-                      (context) => AddAddressPage(
-                        adresler: adresler,
-                        onAddressAdded: _addNewAddress,
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Row(
+                        children: [
+                          Text("Adres Ekle"),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              Navigator.pop(
+                                context,
+                              ); //Dialog penceresini kapatmak için
+                            },
+                            icon: Icon(Icons.close, color: Colors.black),
+                          ),
+                        ],
                       ),
-                ),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              foregroundColor: Colors.blue.shade400,
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      content: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            CustomTextField(
+                              label: "Adres",
+                              onChanged: (address) {
+                                _addressController.text = address;
+                              },
+                              controller: _addressController,
+                              hint: "Adres",
+                            ),
+                            CustomTextField(
+                              label: "Şehir",
+                              onChanged: (city) {
+                                _cityController.text = city;
+                              },
+                              controller: _cityController,
+                              hint: "Şehir",
+                            ),
+                            Button(
+                              text: "Kaydet",
+                              buttonColor: Colors.blue.shade400,
+                              textColor: Colors.white,
+                              fontSize: 15,
+                              onPressed: () {
+                                setState(() {
+                                  Navigator.pop(context); //Dialog kapatmak için
+                                  _saveAddress();
+                                  _addressController.text = "";
+                                  _cityController.text = "";
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              icon: Icon(Icons.add, color: Colors.white),
             ),
-            child: const Text("Ekle"),
           ),
         ],
         leading: IconButton(
@@ -74,7 +144,7 @@ class _AddressPageState extends State<AddressPage> {
           adresler.isEmpty
               ? const Center(
                 child: Text(
-                  "Burada adres bulunamadı",
+                  "Adres bulunamadı",
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
@@ -88,13 +158,15 @@ class _AddressPageState extends State<AddressPage> {
                   final address = adresler[index];
                   return Row(
                     children: [
-                      ListTile(
-                        title: Text(
-                          "Adres: ${address.address}  Şehir: ${address.sehir}",
-                        ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete, color: Colors.red),
-                          onPressed: () => _deleteAddress(address),
+                      Expanded(
+                        child: ListTile(
+                          title: Text(
+                            "Adres: ${address.address}  Şehir: ${address.sehir}",
+                          ),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete, color: Colors.red),
+                            onPressed: () => _deleteAddress(address),
+                          ),
                         ),
                       ),
                     ],
