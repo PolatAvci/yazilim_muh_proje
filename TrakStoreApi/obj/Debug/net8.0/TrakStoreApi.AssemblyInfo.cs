47ec728df5e57b03ccfc45db4f53f3ed60e3c8ae
Polat Avcı <polatavci2222@gmail.com>
2025-04-03 03:04:21 +0300

    eksik dosyalar eklendi

--- TrakStoreApi/obj/Debug/net8.0/TrakStoreApi.AssemblyInfo.cs
+++ TrakStoreApi/obj/Debug/net8.0/TrakStoreApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrakStoreApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fda8b5d172e18ce6f7614fe656aad2a77404684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c81e59842cadbb9f235bce9258f6aff3105c7e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrakStoreApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrakStoreApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
